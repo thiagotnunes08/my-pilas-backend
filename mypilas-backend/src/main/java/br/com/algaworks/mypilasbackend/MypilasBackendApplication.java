2ae6324c76b038afcab48c6b1cd2e9bf7a594963
thiagonunes <thiagonunes@perbank.com.br>
2023-08-21 17:43:42 -0300

    initial commit

---
+++ mypilas-backend/src/main/java/br/com/algaworks/mypilasbackend/MypilasBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.algaworks.mypilasbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypilasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MypilasBackendApplication.class, args);
+	}
+
+}
---
+++ mypilas-backend/src/test/java/br/com/algaworks/mypilasbackend/MypilasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.algaworks.mypilasbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MypilasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
