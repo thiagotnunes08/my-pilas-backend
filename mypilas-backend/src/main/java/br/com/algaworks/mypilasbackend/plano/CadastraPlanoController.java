54ea4b69032202b10b3d2945cdbba2b12298bb91
thiagonunes <thiagonunes@perbank.com.br>
2023-08-22 16:42:40 -0300

    adicionando cross origin

--- mypilas-backend/src/main/java/br/com/algaworks/mypilasbackend/plano/CadastraPlanoController.java
+++ mypilas-backend/src/main/java/br/com/algaworks/mypilasbackend/plano/CadastraPlanoController.java
@@ -1,6 +1,7 @@
 package br.com.algaworks.mypilasbackend.plano;
 
 import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +14,7 @@ public class CadastraPlanoController {
     public CadastraPlanoController(PlanoRepository planoRepository) {
         this.planoRepository = planoRepository;
     }
-
+    @CrossOrigin(origins = "*", allowedHeaders = "*", maxAge = 3600)
     @PostMapping("api/v1/plano")
     public void cadastra(@RequestBody @Valid NovoPlanoRequest request){
 
